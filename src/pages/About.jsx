f9368a9d8d31e4527f17741782079fa3510bb4ba
sachin saroj <91935812+Sachin066@users.noreply.github.com>
2024-12-18 16:41:31 +0530

    Work on Pages

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -9,9 +9,9 @@ const About = () => {
           alt="Founder"
           className="mx-auto rounded-full w-40 h-40"
         />
-        <h2 className="text-2xl font-bold mt-4 text-gray-800">John Doe</h2>
+        <h2 className="text-2xl font-bold mt-4 text-gray-800">Sachin Shravankumar Saroj</h2>
         <p className="mt-2 text-gray-600">
-          John is the visionary behind [Company Name]. With over a decade of
+          Sachin is the visionary behind SK Solutions Pvt Ltd. With over a decade of
           experience in HR and consulting, he is dedicated to empowering
           businesses.
         </p>
